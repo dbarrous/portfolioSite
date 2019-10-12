5f7d64cde2e5f1d65aafcaf0f93a247287b10fc7
Damian Barrous <damianbarrous@Damians-MBP.localdomain>
2019-10-12 18:48:53 -0400

    Add initial button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "../css/components/button.scss";
+import "../css/components/_button.scss";
 
 const Button = (name, mainColor, secColor) => {
   return (
-    <>
-      <a className="btn btn-lg">What have I built?</a>
-    </>
+    <div className="btn-border">
+      <button className="btn btn-lg">What have I built?</button>
+    </div>
   );
 };
 
