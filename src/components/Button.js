997a94c8be5ed0f31156aa1300d6294d19c1744f
Damian Barrous <damianbarrous@Damians-MacBook-Pro.local>
2019-10-12 07:55:21 -0400

    Create initial file structure

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/components/button.scss";
+
+const Button = (name, mainColor, secColor) => {
+  return (
+    <>
+      <a className="btn btn-lg">What have I built?</a>
+    </>
+  );
+};
+
+export default Button;
