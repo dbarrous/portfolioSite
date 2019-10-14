4b6f410c1d375b0a42b39c2f3389330a6f54ee65
Damian Barrous <damianbarrous@Damians-MacBook-Pro.local>
2019-10-14 17:47:09 -0400

    Add Bottom Container of Buttons, improved some styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Picture from "./Picture";
 import Text from "./Text";
+import Button from "./Button";
 import "../css/components/_webpage.scss";
 const App = () => {
   return (
@@ -8,6 +9,31 @@ const App = () => {
       <div className="topContainers">
         <Picture />
         <Text />
+      </div>
+      <div className="bottomContainers">
+        <Button
+          btnText="Resume"
+          specialCssOuter="resume"
+          specialCssInner="resume-inner"
+        />
+
+        <Button
+          btnText="Github"
+          specialCssOuter="github"
+          specialCssInner="github-inner"
+        />
+
+        <Button
+          btnText="LinkedIn"
+          specialCssOuter="linkedin"
+          specialCssInner="linkedin-inner"
+        />
+
+        <Button
+          btnText="Handshake"
+          specialCssOuter="handshake"
+          specialCssInner="handshake-inner"
+        />
       </div>
     </div>
   );
