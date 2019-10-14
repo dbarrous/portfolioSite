566a0d8202a7d34ef394b564670595b79ab5a264
Damian Barrous <damianbarrous@Damians-MacBook-Pro.local>
2019-10-14 12:38:44 -0400

    Added Profile Component

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Button from "./Button";
+import "../css/components/_picture.scss";
 const Picture = () => {
   return (
     <div className="imgButtonContainer">
-      <img className="profilePic" alt="This is a Profile Picture of Myself" />
-      <div className="Button Container">
+      <img
+        className="profilePic"
+        src="profilePic.jpg"
+        alt="This is a Profile Picture of Myself"
+      />
+      <div className="buttonContainer">
         <Button btnText="Who Am I?" />
       </div>
     </div>
